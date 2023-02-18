85c04049f1b7377df91a28a5185540fd7b51e66a
Jing Wei Li <ljw9801055@gmail.com>
2023-02-18 01:21:32 -0500

    Initial Commit

---
+++ BusTime/BusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTimeApp.swift
+//  BusTime
+//
+//  Created by Jing Wei Li on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
